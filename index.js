f67e563d25f38daad5aeb9fbdb0f5b4abd3c20df
gael <gaelzarco@Mac-mini.local>
2022-01-12 17:46:07 -0800

    404 ROUTE! added

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('connected!')
+    res.send('CONNECTED! to server')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 MISSING! content</h1>')
 })
 
 app.listen(process.env.PORT, () => {
