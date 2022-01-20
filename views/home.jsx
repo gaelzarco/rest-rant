cb4cb21be751a4619e46bee76c6e37749586d080
gael <gaelzarco@Mac-mini.local>
2022-01-19 19:34:02 -0800

    added bootstrap grid to index

--- views/home.jsx
+++ views/home.jsx
@@ -3,14 +3,20 @@ const Def = require('./default')
 
 function home () {
     return (
-    <Def>
-        <main>
-            <h1>HOME</h1>
-            <a href="/places">
-                <button className="btn-primary">Places Page</button>
-            </a>
-        </main>
-    </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img style={{width: "80%"}} src="/images/moleconpollo.jpg" alt="Mole with Chicken and Rice!" />
+                  <div>
+                    Photo from <a href="https://www.cardamomo.news/comida/Enmoladas-de-pollo-con-mole-Dona-Maria-y-arroz-receta-facil-20210130-0004.html">Cardamomo.com</a>
+                  </div>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
 }
 
