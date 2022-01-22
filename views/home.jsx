c0fe8e019242ea3c5d013be74fd9ac4e77a56de0
gaelzarco <gaelzarco@yahoo.com>
2022-01-22 01:39:22 -0800

    navbar with style

--- views/home.jsx
+++ views/home.jsx
@@ -5,16 +5,13 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>REST-Rant</h1>
                 <div>
                   <img style={{width: "80%"}} src="/images/moleconpollo.jpg" alt="Mole with Chicken and Rice!" />
                   <div>
                     Photo from <a href="https://www.cardamomo.news/comida/Enmoladas-de-pollo-con-mole-Dona-Maria-y-arroz-receta-facil-20210130-0004.html">Cardamomo.com</a>
                   </div>
                 </div>
-                <a href="/places">
-                    <button className="btn-primary">Places Page</button>
-                </a>
             </main>
         </Def>
     )
