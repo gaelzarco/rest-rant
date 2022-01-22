67a7b5aeb5b7fad3e5ee3e1d892f5d61387f08de
gaelzarco <gaelzarco@yahoo.com>
2022-01-22 03:39:31 -0800

    del functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,43 +1,107 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    if (!req.body.pic) {
-      // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-  })
-
-  // Renders form
+// New
 router.get('/new', (req, res) => {
-    res.render('places/newform')
-  })
+  res.render('places/newform')
+})
 
-  router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
-    }
-    else if (!places[id]) {
-      res.render('error404')
-    }
-    else {
-      res.render('places/show', { place: places[id] })
-    }
-  })
+// Create
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Defaults
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
 
-  // Renders places page
-router.get('/', (req, res) => {
+  // Get /places
+  router.get('/', (req, res) => {
     res.render('places/index', {places})
+})
+
+ // Show
+ router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id], id })
+  }
+})
+
+// Edit
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id], id })
+  }
+})
+
+  // Delete
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
+// Update
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
+// Catchall
+router.post('*', (req, res) => {
+  res.render('error404')
 })
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,30 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <img style={{width: "80%"}} src={ data.place.pic } alt={"image of" && data.place.pic} />
+            <div>
+                <h1>{ data.place.name }</h1>
+            </div>
+            <div>
+                <h2>Rating</h2>
+                <p>N/A</p>
+            </div>
+            <div>
+                <h2>Description</h2>
+                <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
+            </div>
+            <div>
+                <h2>Comments</h2>
+                <p>No Comments Yet</p>
+            </div>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit </a>  
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger"> Delete </button>
+            </form>      
           </main>
         </Def>
     )
